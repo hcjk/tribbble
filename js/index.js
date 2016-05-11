74c30b31cbafef04cb17bebf703c3d0b6cc4b949
Henry Kaufman <hkaufman19@gmail.com>
2016-05-11 09:48:27 -0600

    Updated with links

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 function success(apiResponse) {
     console.log(apiResponse);
-    var num = Math.round(Math.random() * apiResponse.length);
+    var num = Math.round(Math.random() * (apiResponse.length - 1));
     console.log(num);
     $("#shot-img").attr("src", apiResponse[num].images.normal);
-    $("#title").html(apiResponse[num].title);
-    $("#by").html(apiResponse[num].user.name);
+    $("#title").html('<a class="title" href=' + apiResponse[num].html_url + ' target="_blank">' + apiResponse[num].title + '</a>');
+    $("#by").html('<a class="by" href=' + apiResponse[num].user.html_url + ' target="_blank">' + apiResponse[num].user.name + '</a>');
 };
 
 function error(jqxhr) {
