e85db4de3d9c24e38a0194a3bc241ea8838c47dd
Henry Kaufman <hkaufman19@gmail.com>
2016-05-11 10:27:58 -0600

    likes and dislikes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
+var num;
+
+var data;
+
 function success(apiResponse) {
-    console.log(apiResponse);
-    var num = Math.round(Math.random() * (apiResponse.length - 1));
-    console.log(num);
-    $("#shot-img").attr("src", apiResponse[num].images.normal);
-    $("#title").html('<a class="title" href=' + apiResponse[num].html_url + ' target="_blank">' + apiResponse[num].title + '</a>');
-    $("#by").html('<a class="by" href=' + apiResponse[num].user.html_url + ' target="_blank">' + apiResponse[num].user.name + '</a>');
+    num = Math.round(Math.random() * (apiResponse.length - 1))
+    data = apiResponse;
+    $("#shot-img").attr("src", data[num].images.normal);
+    $("#title").html('<a class="title" href=' + data[num].html_url + ' target="_blank">' + data[num].title + '</a>');
+    $("#by").html('<a class="by" href=' + data[num].user.html_url + ' target="_blank">' + data[num].user.name + '</a>');
 };
 
 function error(jqxhr) {
@@ -13,3 +16,33 @@ function error(jqxhr) {
 $.jribbble.setToken('81840dbdbadc6f1e3e5abc584966342b9904b3bb4221a71eb27dad7980b055ae');
 
 $.jribbble.shots().then(success, error);
+
+$('#pass').click(function() {
+    console.log(data);
+    if(data.length > 0) {
+        data.splice(num, 1);
+        num = data.length > 1 ? Math.round(Math.random() * (data.length - 1)) : 0;
+        $("#shot-img").attr("src", data[num].images.normal);
+        $("#title").html('<a class="title" href=' + data[num].html_url + ' target="_blank">' + data[num].title + '</a>');
+        $("#by").html('<a class="by" href=' + data[num].user.html_url + ' target="_blank">' + data[num].user.name + '</a>');
+    }
+    else {
+        $('#maybe-hidden').hide();
+        $('#hidden').show();
+    }
+});
+
+$('#next').click(function() {
+    if(data.length > 0) {
+        data.splice(num, 1);
+        data.splice(num, 1);
+        num = Math.round(Math.random() * (data.length - 1));
+        $("#shot-img").attr("src", data[num].images.normal);
+        $("#title").html('<a class="title" href=' + data[num].html_url + ' target="_blank">' + data[num].title + '</a>');
+        $("#by").html('<a class="by" href=' + data[num].user.html_url + ' target="_blank">' + data[num].user.name + '</a>');
+    }
+    else {
+        $('#maybe-hidden').hide();
+        $('#hidden').show();
+    }
+});
